849495c240fbe8b5b5a564cb425aa09138b443f8
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2023-09-26 23:07:52 +0530

    added new class

--- src/practiceJavaCode/OddEvenCount.java
+++ src/practiceJavaCode/OddEvenCount.java
@@ -2,7 +2,7 @@ package practiceJavaCode;
 
 public class OddEvenCount {
 	public static void main(String Args[]) {
-		int []  A= {11,1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17};
+		int []  A= {11,1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,19,25,29,31,55,11,51,53};
 		int evenCount=0;
 		int oddCount=0;
 		for(int Num : A) {
@@ -14,6 +14,7 @@ public class OddEvenCount {
 		}
 		System.out.println(evenCount);
 		System.out.println(oddCount);
+		
 	}
 
 }
