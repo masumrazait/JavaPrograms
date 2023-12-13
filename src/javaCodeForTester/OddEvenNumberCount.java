b902b7a4fa775f25ef729fb1f39075edc8c801cc
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2023-12-14 00:50:42 +0530

    added new class

---
+++ src/javaCodeForTester/OddEvenNumberCount.java
@@ -0,0 +1,26 @@
+package javaCodeForTester;
+
+import java.util.Scanner;
+
+public class OddEvenNumberCount {
+	public static void main(String Args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int num = sc.nextInt();
+		int odd_count = 0;
+		int even_count = 0;
+		while (num > 0) {
+			int rem = num % 10;
+			if (rem % 2 == 0) {
+				even_count++;
+			} else {
+				odd_count++;
+			}
+			num = num / 10;
+		}
+		System.out.println("Even number count is: " + even_count);
+		System.out.println("Odd number count is: " + odd_count);
+	}
+
+}
