76b76b4cb72dc17f562bfbc0bb3042f3dcc3ff3f
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-01-03 23:08:21 +0530

    class added

--- src/javaCodeForTester/FindUniqLatter.java
+++ src/javaCodeForTester/FindUniqLatter.java
@@ -8,12 +8,11 @@ public class FindUniqLatter {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String : ");
-		String UserInput=sc.nextLine().toLowerCase();
-		Set<Character>uniq=new HashSet<>();
-		for(char c:UserInput.toCharArray()) {
-			if(Character.isLetter(c))
+		String UserInput = sc.nextLine().toLowerCase();
+		Set<Character> uniq = new HashSet<>();
+		for (char c : UserInput.toCharArray())
+			if (Character.isLetter(c))
 				uniq.add(c);
-		}
 		System.out.println(uniq);
 	}
 }
