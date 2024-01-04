87d8b68563588398cf287fec05c53c80a715263e
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-01-05 00:39:55 +0530

    added new class

---
+++ src/javaCodeForTester/Table.java
@@ -0,0 +1,15 @@
+package javaCodeForTester;
+
+import java.util.Scanner;
+
+public class Table {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Num : ");
+		int n=sc.nextInt();
+		for(int i=1;i<=10;i++) {
+			System.out.println(n +"*"+i+"="+ (n*i));
+		}
+	}
+}
