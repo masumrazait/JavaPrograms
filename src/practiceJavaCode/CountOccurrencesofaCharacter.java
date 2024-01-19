e33cbdbc66c62e55950e973630d0564fe8a9c05f
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-01-19 18:36:59 +0530

    code changes

--- src/practiceJavaCode/CountOccurrencesofaCharacter.java
+++ src/practiceJavaCode/CountOccurrencesofaCharacter.java
@@ -8,15 +8,14 @@ public class CountOccurrencesofaCharacter {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String: ");
-		String userinput = sc.nextLine().toLowerCase();
-		int originalCount = userinput.length();
-		System.out.println("Original string is: " + userinput);
-		System.out.println("Original String count is : " + originalCount);
-		String replaceString = userinput.replace("a", "");
-		int replceCount = replaceString.length();
-		System.out.println("Replace string is : " + replaceString);
-		System.out.println("replace string count is : " + replceCount);
-		int CountOccurrences = originalCount - replceCount;
-		System.out.println("originalCount - replceCount is :" + CountOccurrences);
+		String original = sc.nextLine().toLowerCase();
+		int originalCount = original.length();
+		System.out.println("Original String and String count:" + original + ":" + originalCount);
+		String replaceStr = original.replace("a", "");
+		int replaceCount = replaceStr.length();
+		System.out.println("Original String and String count:" + replaceStr + ":" + replaceCount);
+		int CountOccurrencesofaCharacter = originalCount - replaceCount;
+		System.out.println("CountOccurrencesofaCharacter : " + CountOccurrencesofaCharacter);
+
 	}
 }
