882ca0771bbcb5d99be26d15ddd7fbf1f3950d5f
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-01-22 22:12:25 +0530

    code changes

--- src/javaCodeForTester/IndexOfLaterPosion.java
+++ src/javaCodeForTester/IndexOfLaterPosion.java
@@ -6,10 +6,10 @@ public class IndexOfLaterPosion {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the any word:");
-		String UserInput=sc.nextLine().toLowerCase();
+		String UserInput = sc.nextLine().toLowerCase();
 		System.out.println("Enter the Letter to find the Index Position: ");
-		String substring=sc.nextLine().toLowerCase();
-		int Index=UserInput.indexOf(substring);
-		System.out.println(substring+" - index position is :"+ Index);
-		}
-		}
+		String substring = sc.nextLine().toLowerCase();
+		int Index = UserInput.indexOf(substring);
+		System.out.println(substring + " - index position is :" + Index);
+	}
+}
