8035e070ef89c3cecb433e1d5f06c46d52d15ad0
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-01-22 21:45:15 +0530

    code changes

--- src/javaCodeForTester/IndexOfLaterPosion.java
+++ src/javaCodeForTester/IndexOfLaterPosion.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class IndexOfLaterPosion {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the input string:");
-		String inputString = sc.nextLine();
-		System.out.println("Enter the sub string:");
-		String subString = sc.nextLine();
-		int index = inputString.indexOf(subString);
-		System.out.println("Index of sub string is: " + index);
+		System.out.println("Enter the any word:");
+		String UserInput=sc.nextLine().toLowerCase();
+		System.out.println("Enter the Letter to find the Index Position: ");
+		String substring=sc.nextLine().toLowerCase();
+		int Index=UserInput.indexOf(substring);
+		System.out.println(substring+" - index position is :"+ Index);
 		}
 		}
