1a2e9b4db351de926f8c2a78f567cdb8928dc3da
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-01-23 23:48:21 +0530

    code changes

--- src/javaCodeForTester/FibonnacciSeries.java
+++ src/javaCodeForTester/FibonnacciSeries.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class FibonnacciSeries {
 
 	public static void main(String[] args) {
-		int n1 = 0, n2 = 1, sum=0;
+		int n1 = 0, n2 = 1, sum;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number");
-		int userinput = sc.nextInt();
-		System.out.println(n1 + "   " + n2);
-		for (int i = 2; i <= userinput; i++) {
+		System.out.println("Enter the number:");
+		int num = sc.nextInt();
+		System.out.println(n1 + " " + n2);
+		for (int i = 2; i <= num; i++) {
 			sum = n1 + n2;
-			n1 = n2;
-			n2 = sum;
-			System.out.println("series is:"+sum);
+			n2 = n1;
+			n1 = sum;
+			System.out.println(sum);
 		}
 	}
 }
