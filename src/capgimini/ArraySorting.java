c76d78d96b0cfa2b89b347b6d094697c6cb8338f
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-07-23 12:06:29 +0530

    code changes

--- src/capgimini/ArraySorting.java
+++ src/capgimini/ArraySorting.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArraySorting {
 
 	public static void main(String[] args) {
-		int arr[] = { 12, 123, 32, 124, 13, 11, 111, 19 };
+		int arr[] = { 12, 453, 64, 332, 241, 13, 133, 546, 747, 77, 32, 43, 66, 86 };
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
---
+++ src/capgimini/PatternPrinting.java
@@ -0,0 +1,15 @@
+package capgimini;
+
+public class PatternPrinting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
