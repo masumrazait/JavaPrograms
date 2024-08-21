5e7329f31a8ebdd625f515aa6071e348180e1f30
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2024-08-21 21:36:25 +0530

    code changes

---
+++ src/August20/CharacterCount.java
@@ -0,0 +1,16 @@
+package August20;
+
+import java.util.HashMap;
+
+public class CharacterCount {
+
+	public static void main(String[] args) {
+		String str = "masum";
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (char c : str.toCharArray()) {
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		}
+		System.out.println(map);
+	}
+
+}
