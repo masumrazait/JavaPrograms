e5c4b9185875785b92d2827a0ee743023189987f
masumrazait <masumrazait@gmail.com>
2024-12-02 23:43:34 +0530

    code changes

--- src/August1/CharacterCount.java
+++ src/August1/CharacterCount.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class CharacterCount {
 
 	public static void main(String[] args) {
-		String str = "masum";
+		String str = "masumraza";
 		HashMap<Character, Integer> map = new HashMap<>();
 		for (char c : str.toCharArray()) {
 			map.put(c, map.getOrDefault(c, 0) + 1);
