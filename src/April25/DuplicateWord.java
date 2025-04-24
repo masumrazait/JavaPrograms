8f86002f5338212e7795d72ec756fe6582af36cd
masum raza <masumrazait@gmail.com>
2025-04-24 23:18:59 +0530

    added new class

--- src/April25/DuplicateWord.java
+++ src/April25/DuplicateWord.java
@@ -1,10 +1,22 @@
 package April25;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DuplicateWord {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String word="ape,zeep,rose,rose,dell,go,hp,go";
+		String words[]=word.split(",");
+		Set<String>uniq=new HashSet<>();
+		
+		for(String word1:words) {
+			uniq.add(word1.trim());
+			
+		}
+		for (String word1 : uniq) {
+			System.out.print(word1 + ",");
+		}
 	}
 
 }
