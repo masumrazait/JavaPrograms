747e15dbd31233e5439405d163bd2b94a369f5f7
masumrazait <masumrazait@gmail.com>
2025-11-03 22:28:33 +0530

    code changes

--- src/practiceJavaCode/ExceptionExample.java
+++ src/practiceJavaCode/ExceptionExample.java
@@ -3,9 +3,9 @@ package practiceJavaCode;
 public class ExceptionExample {
 	public static void main(String[] args) {
 		try {
-			int[] numbers = { 1, 2, 3 ,4,5,6,8,8,9,5};
+			int[] numbers = { 1, 2, 3, 4, 5, 6, 8, 8, 9, 5 };
 
-		int result = numbers[5]; // This will throw an ArrayIndexOutOfBoundsException
+			int result = numbers[5]; // This will throw an ArrayIndexOutOfBoundsException
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.err.println("Array index out of bounds: " + e.getMessage());
 		} catch (ArithmeticException e) {
